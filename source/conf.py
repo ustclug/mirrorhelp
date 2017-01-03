654c2947867db6ab36c5ac786267980f43823820
Shengjing Zhu <zsj950618@gmail.com>
2017-01-04 01:42:34 +0800

    sphinx: fix language

--- source/conf.py
+++ source/conf.py
@@ -47,7 +47,7 @@ author = 'LUG@USTC'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = 'zh-CN'
+language = 'zh_CN'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
